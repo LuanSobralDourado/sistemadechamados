ba2b397e750aa16950a77da316ce6deb42112df3
Luan Sobral Dourado <luan.sobral@admsepa.ac.gov.br>
2022-01-19 12:51:05 -0500

    Commit inicial

---
+++ layout/template.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Devmix Bootstrap 4 Admin Template">
+    <title>Home</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../images/favicon.png">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="../css/owl.carousel.min.css" rel="stylesheet" type="text/css">
+    <link href="../css/owl.theme.default.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/themify-icons.css">
+    <link href="../css/ionicons.min.css" rel="stylesheet" />
+    <link href="../css/et-line.css" rel="stylesheet" />
+    <link href="css/feather.css" rel="stylesheet" />
+    <script src="../js/modernizr-2.8.3.min.js"></script>
+    <link rel="stylesheet" href="../css/metisMenu.css">
+    <link rel="stylesheet" href="../css/slicknav.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900"
+        rel="stylesheet">
+</head>
+<body>
+    <div class="page-container">
+        <div class="sidebar-menu light-sidebar">
+            <div class="sidebar-header">
+                <div class="logo">
+                    <a href="index.html"><img src="../images/logo-dark.svg" alt="logo"></a>
+                </div>
+            </div>
+            <div class="main-menu">
+                <div class="menu-inner" id="sidebar_menu">
+                    <nav>
+                        <ul class="metismenu" id="menu">
+                            <li>
+                                <a href="template.html" aria-expanded="true">
+                                    <i class="feather ft-home"></i>
+                                    <span>Página inicial</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true">
+                                    <i class="feather ft-home"></i>
+                                    <span>Chamados</span>
+                                </a>
+                                <ul class="collapse">
+                                    <li><a href="#">Abertos</a></li>
+                                    <li><a href="#">Andamento</a></li>
+                                    <li><a href="#">Finalizados</a></li>
+                                </ul>
+                            </li>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="main-content">
+            <div class="header-area">
+                <div class="row align-items-center">
+                    <div class="mobile-logo d_none_lg">
+                        <a href="#"><img src="../images/mobile-logo.svg" alt="logo"></a>
+                    </div>
+                    <div class="col-md-6 d_none_sm d-flex align-items-center">
+                        <div class="nav-btn pull-left">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                        <ul class="notification-area pull-right">
+                            <li>
+                                <span class="nav-btn pull-left d_none_lg">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </span>
+                            </li>
+                            <li id="full-view" class="d_none_sm"><i class="feather ft-maximize"></i></li>
+                            <li id="full-view-exit" class="d_none_sm"><i class="feather ft-minimize"></i></li>
+                            <li class="user-dropdown">
+                                <div class="dropdown">
+                                    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <span class="d_none_sm">Usuário <i class="ti-angle-down"></i></span>
+                                    </button>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <a class="dropdown-item" href="#"><i class="ti-user"></i> Perfil</a>
+
+                                        <a class="dropdown-item" href="../index.html"><i
+                                                class="ti-power-off"></i>Sair</a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="main-content-inner">
+                <P align="center">CONTAINER</P>
+            </div>
+        </div>
+        <footer>
+            <div class="footer-area">
+                <p>SEPA</p>
+            </div>
+        </footer>
+    </div>
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/popper.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/metisMenu.min.js"></script>
+    <script src="../js/jquery.slimscroll.min.js"></script>
+    <script src="../js/jquery.slicknav.min.js"></script>
+    <script src="../js/main.js"></script>
+</body>
+</html>
